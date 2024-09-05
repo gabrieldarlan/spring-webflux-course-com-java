cab8e55bf88b0294d53a640296b99027e86394d5
Gabriel Darlan <gabriel.darlan88@gmail.com>
2024-09-05 19:25:33 -0300

    add test find by id with return not found

--- src/test/java/com/gdarlan/webfluxcourse/controller/UserControllerImplTest.java
+++ src/test/java/com/gdarlan/webfluxcourse/controller/UserControllerImplTest.java
@@ -5,6 +5,7 @@ import com.gdarlan.webfluxcourse.mapper.UserMapper;
 import com.gdarlan.webfluxcourse.model.request.UserRequest;
 import com.gdarlan.webfluxcourse.model.response.UserResponse;
 import com.gdarlan.webfluxcourse.service.UserService;
+import com.gdarlan.webfluxcourse.service.exception.ObjectNotFoundException;
 import com.mongodb.reactivestreams.client.MongoClient;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -17,11 +18,14 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
+import static java.lang.String.format;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.web.reactive.function.BodyInserters.fromValue;
+import static reactor.core.publisher.Mono.error;
 import static reactor.core.publisher.Mono.just;
 
 @ExtendWith(SpringExtension.class)
@@ -104,6 +108,26 @@ class UserControllerImplTest {
                 .jsonPath("$.name").isEqualTo(NAME)
                 .jsonPath("$.email").isEqualTo(EMAIL)
                 .jsonPath("$.password").isEqualTo(PASSWORD);
+
+    }
+
+    @Test
+    @DisplayName("Test find by id endpoint with return not found")
+    void testFindByIdWithReturnNotFound() {
+
+        when(service.findById(ID))
+                .thenReturn(error(new ObjectNotFoundException(format("Object not found. Id: %s, Type: %s", ID, User.class.getSimpleName()))));
+
+        webTestClient
+                .get()
+                .uri("/users/" + ID)
+                .accept(APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isNotFound()
+                .expectBody()
+                .jsonPath("$.status").isEqualTo(NOT_FOUND.value())
+                .jsonPath("$.error").isEqualTo("Not Found")
+                .jsonPath("$.message").isEqualTo("Object not found. Id: " + ID + ", Type: User");
 
     }
 
