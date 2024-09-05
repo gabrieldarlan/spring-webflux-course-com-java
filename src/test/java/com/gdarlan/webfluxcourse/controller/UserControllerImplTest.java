fd8960a543fcfc1b13bab45b72785aa8b33040d3
Gabriel Darlan <gabriel.darlan88@gmail.com>
2024-09-05 19:31:35 -0300

    add test find all with success

--- src/test/java/com/gdarlan/webfluxcourse/controller/UserControllerImplTest.java
+++ src/test/java/com/gdarlan/webfluxcourse/controller/UserControllerImplTest.java
@@ -17,6 +17,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
 
 import static java.lang.String.format;
 import static org.mockito.ArgumentMatchers.any;
@@ -132,7 +133,24 @@ class UserControllerImplTest {
     }
 
     @Test
-    void findAll() {
+    @DisplayName("Test find all endpoint with success")
+    void testFindAllWithSuccess() {
+        final var userResponse = new UserResponse(ID, NAME, EMAIL, PASSWORD);
+
+        when(service.findAll()).thenReturn(Flux.just((User.builder().build())));
+        when(mapper.toResponse(any(User.class))).thenReturn(userResponse);
+
+        webTestClient
+                .get()
+                .uri("/users")
+                .accept(APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody()
+                .jsonPath("$.[0].id").isEqualTo(ID)
+                .jsonPath("$.[0].name").isEqualTo(NAME)
+                .jsonPath("$.[0].email").isEqualTo(EMAIL)
+                .jsonPath("$.[0].password").isEqualTo(PASSWORD);
     }
 
     @Test
